4ee25e2e109471153ecd6f16ed51d3db2aa4e7c6
Srajan <100074395+SrajanDongre@users.noreply.github.com>
2022-08-14 11:58:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import FormikCom from "./FormikCom";
+import Formik from "./Formik";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <FormikCom />
+    <Formik />
   </StrictMode>
 );
